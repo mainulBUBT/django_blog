3915438cf110214b0328b4842adf467a28088d23
mainulBUBT <shopniilmihan@gmail.com>
2022-04-01 12:22:46 +0600

    update

--- Blog/App_Login/forms.py
+++ Blog/App_Login/forms.py
@@ -1,5 +1,3 @@
-from dataclasses import fields
-import email
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
